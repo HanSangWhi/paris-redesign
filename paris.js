366c3d092270096f215328c2068d02e3feb62e50
HanSangWhi <vlrmvka1234@gmail.com>
2022-06-10 16:18:20 +0900

    섹션4 틀잡기

--- paris.js
+++ paris.js
@@ -1,16 +1,69 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const section1swiper = new Swiper('.section1 .swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const section2swiper = new Swiper('.section4>.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  effect:"coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect:{
+    rotate: 50,
+    stretch:0,
+    depth:100,
+    modifier:1,
+    slideShadow: true,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiper1 = new Swiper('.bread-box .swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 0,
+  // 화살표
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.cake-box .swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 0,
+  // 화살표
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper3 = new Swiper('.sandwich-box .swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 0,
+  // 화살표
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
